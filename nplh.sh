bccf7e8d23197e2b4ab8ef0dd90f5083248bc429
Rodrez <fabian.rodrez@gmail.com>
2024-01-20 02:05:21 -0600

    Added echos to nplh

--- nplh.sh
+++ nplh.sh
@@ -1,10 +1,14 @@
+echo "Cloninig bare repo"
 git clone --bare git@github.com:rodrez/dotfiles.git $HOME/.dotfiles
 
 function config {
     /usr/bin/git --git-dir=$HOME/.dotfiles/ $@
 }
 
+echo "Creating config backup dir"
 mkdir -p .config-backup
+
+echo "Initial checkout"
 config checkout
 if [ $? = 0]; then
     echo "Config checked out"
@@ -13,5 +17,8 @@ else
     config checkout 2>&1 | grep -E "\s+\." | awk {'print $1'} | xargs -I {} mv .config-backup/{}
 fi;
 
+echo "Final checkout"
 config checkout 
+
+echo "Hiding untrack files from config(git)"
 config config status.showUntrackedFiles no
